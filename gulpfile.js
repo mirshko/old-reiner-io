f7dd0892308dab3b8a4a03dde8b4813ed93469fd
mirshko <theundeadghost@gmail.com>
2017-02-18 10:12:36 -0500

    :art: Lots of pretty-ness
    Also cool new link hover effect
    
    Signed-off-by: mirshko <theundeadghost@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,41 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var surge = require('gulp-surge')
 
 
 // DEFAULT
 gulp.task('default', ['sass', 'browser-sync'], function() {
-    gulp.watch("./sass/**/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+	gulp.watch("./sass/**/*.scss", ['sass']);
+	gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // BROWSER-SYNC
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
 });
 
 // COMPILE SASS
 gulp.task('sass', function() {
-    return gulp.src("./sass/**/*.scss")
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest("./dist"))
-        .pipe(browserSync.stream());
+	return gulp.src("./sass/**/*.scss")
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("./dist"))
+		.pipe(browserSync.stream());
 });
 
 // DEPLOY TO SURGE.SH
 gulp.task('deploy', [], function() {
-    return surge({
-        project: './', // PATH TO YOUR STATIC BUILD DIRECTORY
-        domain: 'reiner.io' // YOUR DOMAIN OR SURGE SUBDOMAIN
-    });
+	return surge({
+		project: './', // PATH TO YOUR STATIC BUILD DIRECTORY
+		domain: 'reiner.io' // YOUR DOMAIN OR SURGE SUBDOMAIN
+	});
 });
