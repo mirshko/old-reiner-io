6bd840697416f1edc7e1aa3d7d8c37e2c0d34502
mirshko <theundeadghost@gmail.com>
2017-02-24 14:21:00 -0500

    Cleaning up Gulpfile, improving dev process

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
+// **********
+// ** Gulp **
+// **********
+
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var rename = require('gulp-rename');
+var surge = require('gulp-surge');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var surge = require('gulp-surge')
+var uglify = require('gulp-uglify');
+var xo = require('gulp-xo');
 
+// ***********
+// ** Tasks **
+// ***********
 
 // DEFAULT
-gulp.task('default', ['sass', 'browser-sync'], function() {
-	gulp.watch("./sass/**/*.scss", ['sass']);
+gulp.task('default', ['styles', 'scripts', 'browser-sync'], function() {
+	gulp.watch("./scss/**/*.scss", ['styles']);
+	gulp.watch('./js/**/*.js', ['scripts']);
 	gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
@@ -20,20 +31,33 @@ gulp.task('browser-sync', function() {
 	});
 });
 
-// COMPILE SASS
-gulp.task('sass', function() {
-	return gulp.src("./sass/**/*.scss")
-		.pipe(sourcemaps.init())
+// SCRIPTS
+gulp.task('scripts', function () {
+	return gulp.src('js/**/*.js')
+		.pipe(xo())
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist/'));
+});
+
+// STYLES
+gulp.task('styles', function () {
+	return gulp.src('./scss/**/*.scss')
+		pipe(sourcemaps.init())
 		.pipe(sass({
 			outputStyle: 'compressed'
 		}).on('error', sass.logError))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest("./dist"))
-		.pipe(browserSync.stream());
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist/'));
 });
 
-// DEPLOY TO SURGE.SH
-gulp.task('deploy', [], function() {
+// DEPLOY
+gulp.task('deploy', function() {
 	return surge({
 		project: './', // PATH TO YOUR STATIC BUILD DIRECTORY
 		domain: 'reiner.io' // YOUR DOMAIN OR SURGE SUBDOMAIN
